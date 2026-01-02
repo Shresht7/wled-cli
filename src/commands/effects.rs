23ddba5bbd825a8d5c4950ec6bf246690a2a1ace
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-01-02 19:36:23 +0530

    Create the `effects` command
    Lists all available effects on the WLED device

---
+++ src/commands/effects.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+use crate::context::Context;
+
+/// List all available effects
+#[derive(Parser, Debug)]
+#[clap(alias = "fx")]
+pub(crate) struct Effects {}
+
+impl Effects {
+    pub(crate) fn execute(&self, ctx: &Context) -> Result<(), Box<dyn std::error::Error>> {
+        let url = format!("http://{}/json/eff", ctx.host);
+
+        let response = ctx.client.get(url).send()?;
+
+        if !response.status().is_success() {
+            return Err(Box::new(response.error_for_status().unwrap_err()));
+        }
+
+        let effects: Vec<String> = response.json()?;
+
+        for fx in effects {
+            println!("{fx}");
+        }
+
+        Ok(())
+    }
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,8 @@
 mod brightness;
 pub(crate) use brightness::Brightness;
 
+mod effects;
+pub(crate) use effects::Effects;
+
 mod power;
 pub(crate) use power::Power;
