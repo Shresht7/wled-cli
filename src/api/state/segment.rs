85265a56fec67680dfad8160c5ef23562becac1b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-01-02 22:12:35 +0530

    Implement `deserialize` for `Segment` struct and improve field definitions
    Implement custom `serde::Deserialize` and `std::fmt::Display` logic for `Segment` struct.
    
    Make all fields `Option`al to conform to the WLED api as the JSON will only contain relevant fields.
    
    Use `snake_case` for fields in Rust code and `camelCase` when serializing/deserializing.
    
    Skip serialization of optional fields

--- src/api/state/segment.rs
+++ src/api/state/segment.rs
@@ -1,37 +1,58 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 pub type Segments = Vec<Segment>;
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Segment {
     /// Zero-indexed ID of the segment. If omitted, the ID will be inferred from the position in the `seg` array
-    id: Option<u8>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<u8>,
 
     /// The LED that the segment starts at.
-    start: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub start: Option<u16>,
 
     /// The LED that the segment ends at.
-    stop: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stop: Option<u16>,
 
     /// Start row from top-left corner of the matrix.
-    startY: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub start_y: Option<u16>,
 
     /// Stop row from the top-left corner of the matrix.
-    stopY: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stop_y: Option<u16>,
 
     /// The total length of the segment. Omitted if `stop` is being returned.
-    len: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub len: Option<u32>,
 
-    fx: Fx,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fx: Option<Fx>,
     // TODO: A whole lot more properties
 }
 
 // ? Going a little overboard with u8s here. Should review at some point.
 
-enum Fx {
+#[derive(Debug)]
+pub enum Fx {
     ID(u8),
     Increment,
     Decrement,
     Random,
+}
+
+impl std::fmt::Display for Fx {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Fx::ID(id) => write!(f, "{id}"),
+            Fx::Increment => write!(f, "~"),
+            Fx::Decrement => write!(f, "~-"),
+            Fx::Random => write!(f, "r"),
+        }
+    }
 }
 
 impl Serialize for Fx {
@@ -41,9 +62,55 @@ impl Serialize for Fx {
     {
         match self {
             Fx::ID(id) => serializer.serialize_u8(*id),
+            // ? Can try &self.to_string() here instead of duplicating ~, ~- and r.
             Fx::Increment => serializer.serialize_str("~"),
             Fx::Decrement => serializer.serialize_str("~-"),
             Fx::Random => serializer.serialize_str("r"),
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for Fx {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct FxVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for FxVisitor {
+            type Value = Fx;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("an integer ID or a string (~, ~-, r)")
+            }
+
+            fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Fx::ID(value as u8))
+            }
+
+            fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Fx::ID(value as u8))
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                match value {
+                    "~" => Ok(Fx::Increment),
+                    "~-" => Ok(Fx::Decrement),
+                    "r" => Ok(Fx::Random),
+                    _ => Err(E::custom(format!("Unknown string value for Fx: {}", value))),
+                }
+            }
+        }
+
+        deserializer.deserialize_any(FxVisitor)
     }
 }
