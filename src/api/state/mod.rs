288e10998a0a0e9ff353d93a983e225d9a5e1208
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-01-02 22:13:20 +0530

    Derive `Serialize` and `Deserialize` implementations for `State`

--- src/api/state/mod.rs
+++ src/api/state/mod.rs
@@ -3,9 +3,20 @@ pub use power::*;
 
 mod segment;
 pub use segment::*;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
 pub struct State {
-    on: PowerState,
-    bri: u8,
-    seg: Segments,
+    /// Whether the device's power is on or off
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub on: Option<PowerState>,
+
+    /// The brightness level of the device. Ranges from 0 to 255.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bri: Option<u8>,
+
+    /// An array of LED segments. Segments are groups of LEDs on the device.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub seg: Option<Segments>,
+    // TODO: The rest of the stuff
 }
